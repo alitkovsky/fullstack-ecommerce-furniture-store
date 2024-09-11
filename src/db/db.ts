3c5952259efdcec04960f1df3bc785faf4f119e7
alitkovsky <alitkovsky@me.com>
2024-09-11 14:47:46 +0200

    Continuing the experiment with Authentification.

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,15 +1,27 @@
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
 
-const prismaClientSingleton = () => {
-  return new PrismaClient()
-}
+// const prismaClientSingleton = () => {
+//   return new PrismaClient()
+// }
+
+// declare global {
+//   var prismaGlobal: ReturnType<typeof prismaClientSingleton> | undefined
+// }
+
+// const db = globalThis.prismaGlobal || prismaClientSingleton();
+
+// if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = db
+
+// export default db;
+
+import { PrismaClient } from '@prisma/client'
 
 declare global {
-  var prismaGlobal: ReturnType<typeof prismaClientSingleton> | undefined
+  var prisma: PrismaClient | undefined
 }
 
-const db = globalThis.prismaGlobal ||prismaClientSingleton()
+export const db = globalThis.prisma || new PrismaClient()
 
-if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = db
+if (process.env.NODE_ENV !== "production") globalThis.prisma = db
 
 export default db;
