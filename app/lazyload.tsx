1bb0e9342e735cb8d76dd0c0d3a3fb9f8d94dbac
alitkovsky <alitkovsky@me.com>
2024-11-27 20:51:47 +0100

    Added the Clerk auth dependencies.

--- app/lazyload.tsx
+++ app/lazyload.tsx
@@ -1,22 +1,20 @@
 import { lazy } from "react";
 
-import Login from "@/app/sign-in/[[...sign-in]]/page";
-import Signup from "@/app/sign-up/[[...sign-up]]/page";
-const BlogPage = lazy(() => import("@/app/blog/page"))
-const CartPage = lazy(() => import("@/app/cart/page"))
-const CheckoutPage = lazy(() => import("@/app/checkout/page"))
-const ContactPage = lazy(() => import("@/app/contact/page"))
-const HomePage = lazy(() => import("@/app/home/page"))
-const NotFound = lazy(() => import("@/app/notfound/page"))
-const ProductPage = lazy(() => import("@/app/products/[id]/page"))
-const WishlistPage = lazy(() => import("@/app/wishlist/page"))
-const ShopPage = lazy(() => import("@/app/shop/page"))
-const LoadingPage = lazy(() => import("@/app/loading/page"))
-const SearchPage = lazy(() => import("@/app/search/page"))
+const AuthPage = lazy(() => import("@/app/auth/[[...auth]]/page"));
+const BlogPage = lazy(() => import("@/app/blog/page"));
+const CartPage = lazy(() => import("@/app/cart/page"));
+const CheckoutPage = lazy(() => import("@/app/checkout/page"));
+const ContactPage = lazy(() => import("@/app/contact/page"));
+const HomePage = lazy(() => import("@/app/home/page"));
+const NotFound = lazy(() => import("@/app/notfound/page"));
+const ProductPage = lazy(() => import("@/app/products/[id]/page"));
+const WishlistPage = lazy(() => import("@/app/wishlist/page"));
+const ShopPage = lazy(() => import("@/app/shop/page"));
+const LoadingPage = lazy(() => import("@/app/loading/page"));
+const SearchPage = lazy(() => import("@/app/search/page"));
 
 export {
-    Login,
-    Signup,
+    AuthPage,
     BlogPage,
     CartPage,
     CheckoutPage,
@@ -28,4 +26,4 @@ export {
     ShopPage,
     LoadingPage,
     SearchPage
-}
+};
